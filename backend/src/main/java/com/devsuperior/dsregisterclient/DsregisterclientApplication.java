1ea896ac6fc697663823cd972ed7fe0dbaad9d12
Paulo Assis <phanet@gmail.com>
2021-02-03 23:07:13 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsregisterclient/DsregisterclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsregisterclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsregisterclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsregisterclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsregisterclient/DsregisterclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsregisterclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsregisterclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
